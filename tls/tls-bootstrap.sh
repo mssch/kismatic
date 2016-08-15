8293f163c4f053ac726944d1db921e039b3437a0
dkoshkin <dkoshkin@apprenda.com>
2016-08-15 16:59:12 -0400

    [KIS-16] Started work on etcd playbook

---
+++ tls/tls-bootstrap.sh
@@ -0,0 +1,7 @@
+cfssl gencert -initca ca-csr.json | cfssljson -bare ca -
+cfssl gencert \
+  -ca=ca.pem \
+  -ca-key=ca-key.pem \
+  -config=ca-config.json \
+  -profile=kubernetes \
+  kubernetes-csr.json | cfssljson -bare kubernetes
