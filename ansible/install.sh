fbff535f9807e7d85f6bdf96b55318f2fe2d91de
dkoshkin <dkoshkin@apprenda.com>
2016-08-24 17:09:28 -0400

    Paginate packet response

--- ansible/install.sh
+++ ansible/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd tls/
-curl -H 'X-Auth-Token: CuUyhwHviRwHxJk3qruZH8ui9V3EN2Rr' https://api.packet.net/projects/001400bb-f3e4-46c1-bbf4-7bbf9ab849ad/devices | jq -r '.devices|=sort_by(.hostname)|.devices[]|.hostname+","+.ip_addresses[0].address+","+.ip_addresses[2].address' > servers
+curl -H 'X-Auth-Token: CuUyhwHviRwHxJk3qruZH8ui9V3EN2Rr' https://api.packet.net/projects/001400bb-f3e4-46c1-bbf4-7bbf9ab849ad/devices\?per_page\=100 | jq -r '.devices|=sort_by(.hostname)|.devices[]|.hostname+","+.ip_addresses[0].address+","+.ip_addresses[2].address' > servers
 ./tls-bootstrap.sh
 cd ../
 if [[ -z "$var" ]]; then
