af7846dc8d26ec7c85b35526038ce78ac5f096d5
Alexander Brand <alexbrand09@gmail.com>
2016-08-30 12:36:34 -0400

    No Ticket - Clarify usage message of version command

--- pkg/cli/version.go
+++ pkg/cli/version.go
@@ -11,7 +11,7 @@ import (
 func NewCmdVersion(version string, out io.Writer) *cobra.Command {
 	return &cobra.Command{
 		Use:   "version",
-		Short: "display the Kismatic version",
+		Short: "display the Kismatic CLI version",
 		Run: func(cmd *cobra.Command, args []string) {
 			fmt.Fprintf(out, "Kismatic version: %s\n", version)
 		},
