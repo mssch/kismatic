240cfcea88f1f2a3b100ce18a0f642dc76ea0390
dkoshkin <dkoshkin@apprenda.com>
2016-09-14 13:08:37 -0400

    KIS-42: Cleaned up docs in printer

--- pkg/util/printer.go
+++ pkg/util/printer.go
@@ -20,57 +20,57 @@ var green = color.New(color.FgGreen)
 var red = color.New(color.FgRed)
 var orange = color.New(color.FgRed, color.FgYellow)
 
-// PrettyPrintOkf format output with tabs and color
+// PrettyPrintOkf [OK](Green) with formatted string
 func PrettyPrintOkf(out io.Writer, msg string, a ...interface{}) {
 	print(out, msg, oktype, a...)
 }
 
-// PrettyPrintOk format output with tabs and color
+// PrettyPrintOk [OK](Green)
 func PrettyPrintOk(out io.Writer, msg string) {
 	print(out, msg, oktype)
 }
 
-// PrettyPrintErrf format output with tabs and color
+// PrettyPrintErrf [ERROR](Red) with formatted string
 func PrettyPrintErrf(out io.Writer, msg string, a ...interface{}) {
 	print(out, msg, errtype, a...)
 }
 
-// PrettyPrintErr format output with tabs and color
+// PrettyPrintErr [ERROR](Red)
 func PrettyPrintErr(out io.Writer, msg string) {
 	print(out, msg, errtype)
 }
 
-// PrettyPrintWarnf format output with tabs and color
+// PrettyPrintWarnf [WARNING](Orange) with formatted string
 func PrettyPrintWarnf(out io.Writer, msg string, a ...interface{}) {
 	print(out, msg, warntype, a...)
 }
 
-// PrettyPrintWarn format output with tabs and color
+// PrettyPrintWarn [WARNING](Orange)
 func PrettyPrintWarn(out io.Writer, msg string) {
 	print(out, msg, warntype)
 }
 
-// PrettyPrintf format output with tabs and color
+// PrettyPrintf no type will be displayed, used for just single line printing
 func PrettyPrintf(out io.Writer, msg string, a ...interface{}) {
 	print(out, msg, notype, a...)
 }
 
-// PrettyPrint format output with tabs and color
+// PrettyPrint no type will be displayed, used for just single line printing
 func PrettyPrint(out io.Writer, msg string) {
 	print(out, msg, notype)
 }
 
-// PrintErrorf print whole message
+// PrintErrorf print whole message in error(Red) format
 func PrintErrorf(out io.Writer, msg string, a ...interface{}) {
 	printColor(out, msg, red, a...)
 }
 
-// PrintError print whole message
+// PrintError print whole message in error(Red) format
 func PrintError(out io.Writer, msg string) {
 	printColor(out, msg, red)
 }
 
-// PrintHeader will print header with = character
+// PrintHeader will print header with predifined width
 func PrintHeader(out io.Writer, msg string) {
 	w := tabwriter.NewWriter(out, 84, 0, 0, '=', 0)
 	fmt.Fprintln(w, "")
