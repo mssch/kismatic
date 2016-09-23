9297faacf8e8920810dfecb1634191209712c33e
Alexander Brand <abrand@apprenda.com>
2016-09-23 10:19:13 -0400

    KIS-65: Add file content inspector check

---
+++ pkg/preflight/check/file_content.go
@@ -0,0 +1,40 @@
+package check
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"regexp"
+)
+
+// FileContentCheck runs a search against the contents of the specified file.
+// The SearchString is a regular expression that is in accordance with the
+// RE2 syntax defined by the Go regexp package.
+type FileContentCheck struct {
+	File         string
+	SearchString string
+}
+
+// Name of the check
+func (c FileContentCheck) Name() string {
+	return fmt.Sprintf("Contents of %q match %q", c.File, c.SearchString)
+}
+
+// Check returns nil if the search string was found in the file.
+func (c FileContentCheck) Check() error {
+	if _, err := os.Stat(c.File); os.IsNotExist(err) {
+		return fmt.Errorf("Attempted to validate file %q, but it doesn't exist.", c.File)
+	}
+	r, err := regexp.Compile(c.SearchString)
+	if err != nil {
+		return fmt.Errorf("Invalid search string provided %q: %v", c.SearchString, err)
+	}
+	b, err := ioutil.ReadFile(c.File)
+	if err != nil {
+		return fmt.Errorf("Attempted to validate %q, but got an error: %v", c.File, err)
+	}
+	if !r.Match(b) {
+		return fmt.Errorf("Searched %q with the expression %q, but no matches were found.", c.File, c.SearchString)
+	}
+	return nil
+}
---
+++ pkg/preflight/check/file_content_test.go
@@ -0,0 +1,33 @@
+package check
+
+import (
+	"io/ioutil"
+	"testing"
+)
+
+func TestFileContentCheckFileDoesntExist(t *testing.T) {
+	c := FileContentCheck{
+		File:         "doesntExist",
+		SearchString: "foo",
+	}
+	err := c.Check()
+	if err == nil {
+		t.Errorf("Expected an error, but didn't get one")
+	}
+}
+
+func TestFileContentCheck(t *testing.T) {
+	f, err := ioutil.TempFile("", "file-regex-check")
+	if err != nil {
+		t.Fatalf("error creating temp file: %v", err)
+	}
+	fileConts := "hello world\n"
+	f.WriteString(fileConts)
+	c := FileContentCheck{
+		File:         f.Name(),
+		SearchString: "^hello w.*",
+	}
+	if c.Check() != nil {
+		t.Errorf("The check failed when we were expecting success. File content: %s\n Search String: %s", fileConts, c.SearchString)
+	}
+}
