ca42878735ee487929a9687dd9e0f0fdf41e9511
Alexander Brand <abrand@apprenda.com>
2016-09-29 12:55:12 -0400

    KIS-71: Don't export default rule set constant

--- pkg/inspector/rule/rule_set.go
+++ pkg/inspector/rule/rule_set.go
@@ -6,7 +6,7 @@ import (
 )
 
 // DefaultRuleSet is the list of rules that are built into the inspector
-const DefaultRuleSet = `---
+const defaultRuleSet = `---
 - kind: PackageAvailable
   when: ["centos"]
   packageName: somePackage
@@ -25,7 +25,7 @@ const DefaultRuleSet = `---
 
 // DefaultRules returns the list of rules that are built into the inspector
 func DefaultRules() []Rule {
-	rules, err := unmarshalRules([]byte(DefaultRuleSet))
+	rules, err := unmarshalRules([]byte(defaultRuleSet))
 	if err != nil {
 		// The default rules should not contain errors
 		// If they do, panic so that we catch them during tests
@@ -36,7 +36,7 @@ func DefaultRules() []Rule {
 
 // DumpDefaultRules writes the default rule set to a file
 func DumpDefaultRules(file string) error {
-	err := ioutil.WriteFile(file, []byte(DefaultRuleSet), 0644)
+	err := ioutil.WriteFile(file, []byte(defaultRuleSet), 0644)
 	if err != nil {
 		return fmt.Errorf("error writing default rule set to %q: %v", file, err)
 	}
