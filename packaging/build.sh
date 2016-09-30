5c8174d2f43397de5401a8461801749489000d82
dkoshkin <dkoshkin@apprenda.com>
2016-09-30 15:10:28 -0400

    KIS-69: Modified Ansible playbooks to support packages

--- packaging/build.sh
+++ packaging/build.sh
@@ -3,33 +3,33 @@ K8S_VERSION=1.4.0
 DOCKER_VERSION=1.11.2
 
 # build Kubernetes
-# rm -rf out/kubernetes/
-# mkdir -p out/kubernetes/rpm/
-# mkdir -p out/kubernetes/deb/
+rm -rf out/kubernetes/
+mkdir -p out/kubernetes/rpm/
+mkdir -p out/kubernetes/deb/
 # RPMs
-# # master
-# fpm -s dir -n "kubernetes-master" \
-# -p out/kubernetes/rpm/ \
-# -C source/ \
-# -d 'docker-engine = 1.11.2' \
-# -d 'bridge-utils' \
-# -v $K8S_VERSION  \
-# -a x86_64 \
-# -t rpm --rpm-os linux \
-# --license "Apache Software License 2.0" \
-# --maintainer "Apprenda <info@apprenda.com>" \
-# --vendor "Apprenda" \
-# --description "Kubernetes master binaries" \
-# --url "https://apprenda.com/" \
-# kubernetes/apiserver/bin/kube-apiserver=/usr/bin/kube-apiserver \
-# kubernetes/kubelet/bin/kubelet=/usr/bin/kubelet \
-# kubernetes/proxy/bin/kube-proxy=/usr/bin/kube-proxy \
-# kubernetes/scheduler/bin/kube-scheduler=/usr/bin/kube-scheduler \
-# kubernetes/controller-manager/bin/kube-controller-manager=/usr/bin/kube-controller-manager \
-# kubernetes/kubectl/bin/kubectl=/usr/bin/kubectl \
-# networking/ctl/bin/calicoctl=/usr/bin/calicoctl \
-# networking/cni/bin/=/opt/cni/ \
-# images/=/opt/
+# master
+fpm -s dir -n "kubernetes-master" \
+-p out/kubernetes/rpm/ \
+-C source/ \
+-d 'docker-engine = 1.11.2' \
+-d 'bridge-utils' \
+-v $K8S_VERSION  \
+-a x86_64 \
+-t rpm --rpm-os linux \
+--license "Apache Software License 2.0" \
+--maintainer "Apprenda <info@apprenda.com>" \
+--vendor "Apprenda" \
+--description "Kubernetes master binaries" \
+--url "https://apprenda.com/" \
+kubernetes/apiserver/bin/kube-apiserver=/usr/bin/kube-apiserver \
+kubernetes/kubelet/bin/kubelet=/usr/bin/kubelet \
+kubernetes/proxy/bin/kube-proxy=/usr/bin/kube-proxy \
+kubernetes/scheduler/bin/kube-scheduler=/usr/bin/kube-scheduler \
+kubernetes/controller-manager/bin/kube-controller-manager=/usr/bin/kube-controller-manager \
+kubernetes/kubectl/bin/kubectl=/usr/bin/kubectl \
+networking/ctl/bin/calicoctl=/usr/bin/calicoctl \
+networking/cni/bin/=/opt/cni/ \
+images/=/opt/
 # worker
 fpm -s dir -n "kubernetes-node" \
 -p out/kubernetes/rpm/ \
@@ -49,103 +49,103 @@ kubernetes/proxy/bin/kube-proxy=/usr/bin/kube-proxy \
 networking/ctl/bin/calicoctl=/usr/bin/calicoctl \
 networking/cni/bin/=/opt/cni/
 
-# #deb
-# # master
-# fpm -s dir -n "kubernetes-master" \
-# -p out/kubernetes/deb/ \
-# -C source/ \
-# -d 'docker-engine = 1.11.2-0~xenial' \
-# -d 'bridge-utils' \
-# -v $K8S_VERSION  \
-# -a amd64 \
-# -t deb \
-# --license "Apache Software License 2.0" \
-# --maintainer "Apprenda <info@apprenda.com>" \
-# --vendor "Apprenda" \
-# --description "Kubernetes master binaries" \
-# --url "https://apprenda.com/" \
-# kubernetes/apiserver/bin/kube-apiserver=/usr/bin/kube-apiserver \
-# kubernetes/kubelet/bin/kubelet=/usr/bin/kubelet \
-# kubernetes/proxy/bin/kube-proxy=/usr/bin/kube-proxy \
-# kubernetes/scheduler/bin/kube-scheduler=/usr/bin/kube-scheduler \
-# kubernetes/controller-manager/bin/kube-controller-manager=/usr/bin/kube-controller-manager \
-# kubernetes/kubectl/bin/kubectl=/usr/bin/kubectl \
-# networking/ctl/bin/calicoctl=/usr/bin/calicoctl \
-# networking/cni/bin/=/opt/cni/ \
-# images/=/opt/
-# # worker
-# fpm -s dir -n "kubernetes-node" \
-# -p out/kubernetes/deb/ \
-# -C source/ \
-# -d 'docker-engine = 1.11.2-0~xenial' \
-# -d 'bridge-utils' \
-# -v $K8S_VERSION  \
-# -a amd64 \
-# -t deb \
-# --license "Apache Software License 2.0" \
-# --maintainer "Apprenda <info@apprenda.com>" \
-# --vendor "Apprenda" \
-# --description "Kubernetes node binaries" \
-# --url "https://apprenda.com/" \
-# kubernetes/kubelet/bin/kubelet=/usr/bin/kubelet \
-# kubernetes/proxy/bin/kube-proxy=/usr/bin/kube-proxy \
-# networking/ctl/bin/calicoctl=/usr/bin/calicoctl \
-# networking/cni/bin/=/opt/cni/
-#
-# # build etcd
-# rm -rf out/etcd/
-# mkdir -p out/etcd/rpm/
-# mkdir -p out/etcd/deb/
-# # RPMs
-# fpm -s dir -n "etcd" \
-# -p out/etcd/rpm/ \
-# -C source/ \
-# -v $K8S_VERSION \
-# -a x86_64 \
-# -t rpm --rpm-os linux \
-# --license "Apache Software License 2.0" \
-# --maintainer "Apprenda <info@apprenda.com>" \
-# --vendor "Apprenda" \
-# --description "Etcd kubernetes and networking binaries" \
-# --url "https://apprenda.com/" \
-# etcd/k8s/bin/etcdk8s=/usr/bin/etcdk8s \
-# etcd/k8s/bin/etcdctlk8s=/usr/bin/etcdctlk8s \
-# etcd/networking/bin/etcdnetworking=/usr/bin/etcdnetworking \
-# etcd/networking/bin/etcdctlnetworking=/usr/bin/etcdctlnetworking
-#
-# #deb
-# fpm -s dir -n "etcd" \
-# -p out/etcd/deb/ \
-# -C source/ \
-# -v $K8S_VERSION  \
-# -a amd64 \
-# -t deb \
-# --license "Apache Software License 2.0" \
-# --maintainer "Apprenda <info@apprenda.com>" \
-# --vendor "Apprenda" \
-# --description "Etcd kubernetes and networking binaries" \
-# --url "https://apprenda.com/" \
-# etcd/k8s/bin/etcdk8s=/usr/bin/etcdk8s \
-# etcd/k8s/bin/etcdctlk8s=/usr/bin/etcdctlk8s \
-# etcd/networking/bin/etcdnetworking=/usr/bin/etcdnetworking \
-# etcd/networking/bin/etcdctlnetworking=/usr/bin/etcdctlnetworking
+#deb
+# master
+fpm -s dir -n "kubernetes-master" \
+-p out/kubernetes/deb/ \
+-C source/ \
+-d 'docker-engine = 1.11.2-0~xenial' \
+-d 'bridge-utils' \
+-v $K8S_VERSION  \
+-a amd64 \
+-t deb \
+--license "Apache Software License 2.0" \
+--maintainer "Apprenda <info@apprenda.com>" \
+--vendor "Apprenda" \
+--description "Kubernetes master binaries" \
+--url "https://apprenda.com/" \
+kubernetes/apiserver/bin/kube-apiserver=/usr/bin/kube-apiserver \
+kubernetes/kubelet/bin/kubelet=/usr/bin/kubelet \
+kubernetes/proxy/bin/kube-proxy=/usr/bin/kube-proxy \
+kubernetes/scheduler/bin/kube-scheduler=/usr/bin/kube-scheduler \
+kubernetes/controller-manager/bin/kube-controller-manager=/usr/bin/kube-controller-manager \
+kubernetes/kubectl/bin/kubectl=/usr/bin/kubectl \
+networking/ctl/bin/calicoctl=/usr/bin/calicoctl \
+networking/cni/bin/=/opt/cni/ \
+images/=/opt/
+# worker
+fpm -s dir -n "kubernetes-node" \
+-p out/kubernetes/deb/ \
+-C source/ \
+-d 'docker-engine = 1.11.2-0~xenial' \
+-d 'bridge-utils' \
+-v $K8S_VERSION  \
+-a amd64 \
+-t deb \
+--license "Apache Software License 2.0" \
+--maintainer "Apprenda <info@apprenda.com>" \
+--vendor "Apprenda" \
+--description "Kubernetes node binaries" \
+--url "https://apprenda.com/" \
+kubernetes/kubelet/bin/kubelet=/usr/bin/kubelet \
+kubernetes/proxy/bin/kube-proxy=/usr/bin/kube-proxy \
+networking/ctl/bin/calicoctl=/usr/bin/calicoctl \
+networking/cni/bin/=/opt/cni/
 
-# # build docker
-# #RPMS
-# rm -rf out/docker/
-# mkdir -p out/docker/rpm/
-# fpm -s rpm -n "docker-engine" \
-# -p out/docker/rpm/ \
-# -v $DOCKER_VERSION \
-# -a x86_64 \
-# -t rpm --rpm-os linux \
-# --license "Apache Software License 2.0" \
-# --maintainer "Apprenda <info@apprenda.com>" \
-# --vendor "Apprenda" \
-# --description "Docker and its dependencies" \
-# --url "https://apprenda.com/" \
-# source/docker/rpm/docker-engine-selinux-1.11.2-1.el7.centos.noarch.rpm \
-# source/docker/rpm/docker-engine-1.11.2-1.el7.centos.x86_64.rpm
-# #deb
-# mkdir -p out/docker/deb/
-# cp source/docker/deb/* out/docker/deb/
+# build etcd
+rm -rf out/etcd/
+mkdir -p out/etcd/rpm/
+mkdir -p out/etcd/deb/
+# RPMs
+fpm -s dir -n "etcd" \
+-p out/etcd/rpm/ \
+-C source/ \
+-v $K8S_VERSION \
+-a x86_64 \
+-t rpm --rpm-os linux \
+--license "Apache Software License 2.0" \
+--maintainer "Apprenda <info@apprenda.com>" \
+--vendor "Apprenda" \
+--description "Etcd kubernetes and networking binaries" \
+--url "https://apprenda.com/" \
+etcd/k8s/bin/etcdk8s=/usr/bin/etcdk8s \
+etcd/k8s/bin/etcdctlk8s=/usr/bin/etcdctlk8s \
+etcd/networking/bin/etcdnetworking=/usr/bin/etcdnetworking \
+etcd/networking/bin/etcdctlnetworking=/usr/bin/etcdctlnetworking
+
+#deb
+fpm -s dir -n "etcd" \
+-p out/etcd/deb/ \
+-C source/ \
+-v $K8S_VERSION  \
+-a amd64 \
+-t deb \
+--license "Apache Software License 2.0" \
+--maintainer "Apprenda <info@apprenda.com>" \
+--vendor "Apprenda" \
+--description "Etcd kubernetes and networking binaries" \
+--url "https://apprenda.com/" \
+etcd/k8s/bin/etcdk8s=/usr/bin/etcdk8s \
+etcd/k8s/bin/etcdctlk8s=/usr/bin/etcdctlk8s \
+etcd/networking/bin/etcdnetworking=/usr/bin/etcdnetworking \
+etcd/networking/bin/etcdctlnetworking=/usr/bin/etcdctlnetworking
+
+# build docker
+#RPMS
+rm -rf out/docker/
+mkdir -p out/docker/rpm/
+fpm -s rpm -n "docker-engine" \
+-p out/docker/rpm/ \
+-v $DOCKER_VERSION \
+-a x86_64 \
+-t rpm --rpm-os linux \
+--license "Apache Software License 2.0" \
+--maintainer "Apprenda <info@apprenda.com>" \
+--vendor "Apprenda" \
+--description "Docker and its dependencies" \
+--url "https://apprenda.com/" \
+source/docker/rpm/docker-engine-selinux-1.11.2-1.el7.centos.noarch.rpm \
+source/docker/rpm/docker-engine-1.11.2-1.el7.centos.x86_64.rpm
+#deb
+mkdir -p out/docker/deb/
+cp source/docker/deb/* out/docker/deb/
--- packaging/getbinaries.sh
+++ packaging/getbinaries.sh
@@ -34,18 +34,25 @@ wget -P source/docker/deb/ $DOCKER_DEB_URL
 
 # docker images
 DOCKER_IMG=registry:2.5.1
+CALICO_IMG=calico/node:v0.22.0
+CALICO_KUBE_POLICY_CONTROLLER_IMG=calico/kube-policy-controller
 KUBEDNS_IMG=gcr.io/google_containers/kubedns-amd64:1.7
 DNSMAQ_IMG=gcr.io/google_containers/kube-dnsmasq-amd64:1.3
 EXECHEALTHZ_IMG=gcr.io/google_containers/exechealthz-amd64:1.0
-KUBERNETES_DASHBOARD=gcr.io/google_containers/kubernetes-dashboard-arm64:v1.4.0
+KUBERNETES_DASHBOARD_IMG=gcr.io/google_containers/kubernetes-dashboard-amd64:v1.4.0
+# Used internally by k8s
+PAUSE_IMG=gcr.io/google_containers/pause-amd64:3.0
 
 rm -rf source/images
 mkdir -p source/images
 docker pull $DOCKER_IMG && docker save $DOCKER_IMG -o source/images/registry.tar
+docker pull $CALICO_IMG && docker save $CALICO_IMG -o source/images/calico.tar
+docker pull $CALICO_KUBE_POLICY_CONTROLLER_IMG && docker save $CALICO_KUBE_POLICY_CONTROLLER_IMG -o source/images/kube-policy-controller.tar
 docker pull $KUBEDNS_IMG && docker save $KUBEDNS_IMG -o source/images/kubedns.tar
 docker pull $DNSMAQ_IMG && docker save $DNSMAQ_IMG -o source/images/kube-dnsmasq.tar
 docker pull $EXECHEALTHZ_IMG && docker save $EXECHEALTHZ_IMG -o source/images/exechealthz.tar
-docker pull $KUBERNETES_DASHBOARD && docker save $KUBERNETES_DASHBOARD -o source/images/kubernetes-dashboard.tar
+docker pull $KUBERNETES_DASHBOARD_IMG && docker save $KUBERNETES_DASHBOARD_IMG -o source/images/kubernetes-dashboard.tar
+docker pull $PAUSE_IMG && docker save $PAUSE_IMG -o source/images/pause.tar
 
 # download etcd
 ETCD_K8S_URL=https://github.com/coreos/etcd/releases/download/v3.0.10/etcd-v3.0.10-linux-amd64.tar.gz
