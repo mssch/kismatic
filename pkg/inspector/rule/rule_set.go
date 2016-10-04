f9a8b87b369a43a6ee3662717265d29a080ce8c0
Alexander Brand <abrand@apprenda.com>
2016-10-04 17:47:18 -0400

    KIS-13: Initial list of inspector rules

--- pkg/inspector/rule/rule_set.go
+++ pkg/inspector/rule/rule_set.go
@@ -7,20 +7,94 @@ import (
 
 // DefaultRuleSet is the list of rules that are built into the inspector
 const defaultRuleSet = `---
-- kind: PackageAvailable
-  when: ["centos"]
-  packageName: somePackage
-  packageVersion: 1.0
+# Executables required by kubelet
+- kind: ExecutableInPath
+  when: ["worker"]
+  executable: iptables
+- kind: ExecutableInPath
+  when: ["worker"]
+  executable: iptables-save
+- kind: ExecutableInPath
+  when: ["worker"]
+  executable: iptables-restore
 
-- kind: PackageAvailable
-  when: ["ubuntu"]
-  packageName: otherPackage
-  packageVersion: 1.2
-
+# Kubelet depends on glibc
 - kind: PackageInstalled
-  when: []
-  packageName: docker
-  packageVersion: 1.11
+  when: ["ubuntu", "worker"]
+  packageName: libc6
+  packageVersion: ".*"
+- kind: PackageInstalled
+  when: ["centos", "worker"]
+  packageName: glibc.x86_64
+  packageVersion: ".*"
+
+# Ensure python 2.7+ is installed
+- kind: PackageInstalled
+  when: ["centos"]
+  packageName: python.x86_64
+  packageVersion: "^2\\.7"
+- kind: PackageInstalled
+  when: ["ubuntu"]
+  packageName: "^python2\\.7$"
+  packageVersion: ".*"
+
+# Ports used by etcd are available
+- kind: TCPPortAvailable
+  when: ["etcd"]
+  port: 2379
+- kind: TCPPortAvailable
+  when: ["etcd"]
+  port: 6666
+- kind: TCPPortAvailable
+  when: ["etcd"]
+  port: 2380
+- kind: TCPPortAvailable
+  when: ["etcd"]
+  port: 6660
+
+# Ports used by etcd are accessible
+- kind: TCPPortAccessible
+  when: ["etcd"]
+  port: 2379
+  timeout: 5s
+- kind: TCPPortAccessible
+  when: ["etcd"]
+  port: 6666
+  timeout: 5s
+- kind: TCPPortAccessible
+  when: ["etcd"]
+  port: 2379
+  timeout: 5s
+- kind: TCPPortAccessible
+  when: ["etcd"]
+  port: 2380
+  timeout: 5s
+- kind: TCPPortAccessible
+  when: ["etcd"]
+  port: 6666
+  timeout: 5s
+- kind: TCPPortAccessible
+  when: ["etcd"]
+  port: 6660
+  timeout: 5s
+
+# Ports used by K8s master are available
+- kind: TCPPortAvailable
+  when: ["master"]
+  port: 6443
+- kind: TCPPortAvailable
+  when: ["master"]
+  port: 8080
+
+# Ports used by K8s master are accessible
+- kind: TCPPortAccessible
+  when: ["master"]
+  port: 6443
+  timeout: 5s
+- kind: TCPPortAccessible
+  when: ["master"]
+  port: 8080
+  timeout: 5s
 `
 
 // DefaultRules returns the list of rules that are built into the inspector
