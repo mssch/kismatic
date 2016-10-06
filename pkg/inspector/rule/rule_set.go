57a18ecc375c8cdf5fe0e246a22cbacbcf9b655c
Alexander Brand <abrand@apprenda.com>
2016-10-06 14:09:18 -0400

    KIS-13: Clean up default rules

--- pkg/inspector/rule/rule_set.go
+++ pkg/inspector/rule/rule_set.go
@@ -27,26 +27,6 @@ const defaultRuleSet = `---
   when: ["worker"]
   executable: iptables-restore
 
-# Kubelet depends on glibc
-- kind: PackageAvailable
-  when: ["ubuntu", "worker"]
-  packageName: libc6
-  packageVersion: ".*"
-- kind: PackageAvailable
-  when: ["centos", "worker"]
-  packageName: glibc.x86_64
-  packageVersion: ".*"
-
-# Ensure python 2.7+ is installed
-- kind: PackageAvailable
-  when: ["centos"]
-  packageName: python.x86_64
-  packageVersion: "^2\\.7"
-- kind: PackageAvailable
-  when: ["ubuntu"]
-  packageName: "^python2\\.7$"
-  packageVersion: ".*"
-
 # Ports used by etcd are available
 - kind: TCPPortAvailable
   when: ["etcd"]
@@ -72,15 +52,7 @@ const defaultRuleSet = `---
   timeout: 5s
 - kind: TCPPortAccessible
   when: ["etcd"]
-  port: 2379
-  timeout: 5s
-- kind: TCPPortAccessible
-  when: ["etcd"]
   port: 2380
-  timeout: 5s
-- kind: TCPPortAccessible
-  when: ["etcd"]
-  port: 6666
   timeout: 5s
 - kind: TCPPortAccessible
   when: ["etcd"]
@@ -104,6 +76,8 @@ const defaultRuleSet = `---
   when: ["master"]
   port: 8080
   timeout: 5s
+
+# TODO: Add kismatic package checks
 `
 
 // DefaultRules returns the list of rules that are built into the inspector
