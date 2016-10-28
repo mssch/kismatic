8aa264fa850ce8fd4732ecdee57d15f1b955388c
Alexander Brand <abrand@apprenda.com>
2016-10-28 17:23:04 -0400

    Move AWS provisioning code into it's own pkg

---
+++ integration/aws/client_test.go
@@ -0,0 +1,41 @@
+// +build aws_client_integration
+
+package aws
+
+import (
+	"fmt"
+	"os"
+	"testing"
+)
+
+// This isn't really a test, but just a method to exercise
+// the client. Verification is performed by looking at the AWS
+// dashboard.
+func TestClient(t *testing.T) {
+	// Creata a node
+	c := Client{
+		Credentials: Credentials{
+			ID:     os.Getenv("AWS_ID"),
+			Secret: os.Getenv("AWS_CREDENTIALS"),
+			Token:  os.Getenv("AWS_TOKEN"),
+		},
+		Config: ClientConfig{
+			Region:          os.Getenv("AWS_REGION"),
+			SubnetID:        os.Getenv("AWS_SUBNET_ID"),
+			Keyname:         os.Getenv("AWS_KEYNAME"),
+			SecurityGroupID: os.Getenv("AWS_SECURITY_GROUP_ID"),
+		},
+	}
+	nodeID, err := c.CreateNode(Ubuntu1604LTSEast, T2Micro)
+	if err != nil {
+		t.Fatalf("Failed to create node: %v", err)
+	}
+	fmt.Printf("Created a node with ID %q\n", nodeID)
+	node, err := c.GetNode(nodeID)
+	if err != nil {
+		t.Fatalf("Failed to get node details: %v", err)
+	}
+	if err := c.DestroyNodes([]string{nodeID}); err != nil {
+		t.Fatalf("Failed to destroy node: %v", err)
+	}
+}
