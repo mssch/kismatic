90f81b2f6ae25990b5161e197add9b592a99ba14
dkoshkin <dkoshkin@apprenda.com>
2016-11-08 16:26:49 -0500

    Increase Ansible timeout for SSH connections

--- pkg/ansible/runner.go
+++ pkg/ansible/runner.go
@@ -133,6 +133,7 @@ func (r *runner) startPlaybook(playbookFile string, inv Inventory, vars ExtraVar
 	os.Setenv("ANSIBLE_HOST_KEY_CHECKING", "False")
 	os.Setenv("ANSIBLE_CALLBACK_PLUGINS", filepath.Join(r.ansibleDir, "playbooks", "callback"))
 	os.Setenv("ANSIBLE_CALLBACK_WHITELIST", "json_lines")
+	os.Setenv("TIMEOUT", "30")
 
 	// We always want the most verbose output from Ansible. If it's not going to
 	// stdout, it's going to a log file.
