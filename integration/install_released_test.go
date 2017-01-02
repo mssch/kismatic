cf5eb4a39df691de70f9ba89762df10258d9c874
Dimitri Koshkin <dimitri.koshkin@gmail.com>
2017-01-02 09:41:00 -0500

    Bump intall_released_test to v1.1.0

--- integration/install_released_test.go
+++ integration/install_released_test.go
@@ -7,7 +7,7 @@ import (
 	. "github.com/onsi/gomega"
 )
 
-const previousKismaticVersion = "v1.0.3"
+const previousKismaticVersion = "v1.1.0"
 
 // Test a specific released version of Kismatic
 var _ = Describe("Installing with previous version of Kismatic", func() {
