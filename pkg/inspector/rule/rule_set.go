11de04438b43b97cc6c82ccdffefafb1acbabe04
Alexander Brand <abrand@apprenda.com>
2017-01-04 19:37:12 -0500

    Lock down the glusterfs-server version

--- pkg/inspector/rule/rule_set.go
+++ pkg/inspector/rule/rule_set.go
@@ -154,15 +154,15 @@ const defaultRuleSet = `---
 - kind: PackageAvailable
   when: ["storage", "centos"]
   packageName: glusterfs-server
-  packageVersion: ""
+  packageVersion: 3.8.5-1.el7
+- kind: PackageAvailable
+  when: ["storage", "rhel"]
+  packageName: glusterfs-server
+  packageVersion: 3.8.5-1.el7
 - kind: PackageAvailable
   when: ["storage", "ubuntu"]
   packageName: glusterfs-server
-  packageVersion: ""
-- kind: PackageAvailable
-  when: ["storage", "ubuntu"]
-  packageName: nfs-common
-  packageVersion: ""
+  packageVersion: 3.7.6-1ubuntu1
 
 # Ports required for NFS
 - kind: TCPPortAvailable
