f8557f2c8e7aacac106dd88cda5421824d7c5b4b
Alexander Brand <abrand@apprenda.com>
2017-03-02 16:49:44 -0500

    add json support to info cmd

--- pkg/cli/version.go
+++ pkg/cli/version.go
@@ -1,6 +1,7 @@
 package cli
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"runtime"
@@ -9,16 +10,39 @@ import (
 	"github.com/spf13/cobra"
 )
 
+type versionOut struct {
+	Version   string
+	BuildDate string
+	GoVersion string
+}
+
 // NewCmdVersion returns the version command
 func NewCmdVersion(buildDate string, out io.Writer) *cobra.Command {
-	return &cobra.Command{
+	var outFormat string
+	cmd := &cobra.Command{
 		Use:   "version",
 		Short: "display the Kismatic CLI version",
-		Run: func(cmd *cobra.Command, args []string) {
+		RunE: func(cmd *cobra.Command, args []string) error {
+			v := versionOut{
+				Version:   install.AboutKismatic.String(),
+				BuildDate: buildDate,
+				GoVersion: runtime.Version(),
+			}
+			if outFormat == "json" {
+				b, err := json.MarshalIndent(v, "", "    ")
+				if err != nil {
+					return fmt.Errorf("error marshaling data: %v", err)
+				}
+				fmt.Fprintf(out, string(b))
+				return nil
+			}
 			fmt.Fprintln(out, "Kismatic:")
 			fmt.Fprintf(out, "  Version: %s\n", install.AboutKismatic)
 			fmt.Fprintf(out, "  Built: %s\n", buildDate)
 			fmt.Fprintf(out, "  Go Version: %s\n", runtime.Version())
+			return nil
 		},
 	}
+	cmd.Flags().StringVarP(&outFormat, "output", "o", "simple", `output format (options "simple"|"json")`)
+	return cmd
 }
