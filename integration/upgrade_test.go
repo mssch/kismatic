f4e901507a72d1eab25f6f73fcab9c05edaea640
Alexander Brand <abrand@apprenda.com>
2017-04-11 09:28:33 -0400

    Remove upgrade test from KET v1.0.X

--- integration/upgrade_test.go
+++ integration/upgrade_test.go
@@ -205,39 +205,6 @@ var _ = Describe("Upgrade", func() {
 				})
 			})
 		})
-
-		Describe("From KET version v1.0.3", func() {
-			BeforeEach(func() {
-				dir := setupTestWorkingDirWithVersion("v1.0.3")
-				os.Chdir(dir)
-			})
-
-			Context("Using a minikube layout", func() {
-				Context("Using CentOS 7", func() {
-					ItOnAWS("should be upgraded [slow] [upgrade]", func(aws infrastructureProvisioner) {
-						WithMiniInfrastructure(CentOS7, aws, func(node NodeDeets, sshKey string) {
-							installAndUpgradeMinikube(node, sshKey)
-						})
-					})
-				})
-
-				Context("Using Ubuntu 16.04", func() {
-					ItOnAWS("should be upgraded [slow] [upgrade]", func(aws infrastructureProvisioner) {
-						WithMiniInfrastructure(Ubuntu1604LTS, aws, func(node NodeDeets, sshKey string) {
-							installAndUpgradeMinikube(node, sshKey)
-						})
-					})
-				})
-			})
-
-			Context("Using a larger cluster layout with RedHat 7", func() {
-				ItOnAWS("should result in an upgraded cluster [slow] [upgrade]", func(aws infrastructureProvisioner) {
-					WithInfrastructureAndDNS(NodeCount{Etcd: 3, Master: 2, Worker: 3, Ingress: 0, Storage: 0}, RedHat7, aws, func(nodes provisionedNodes, sshKey string) {
-						installAndUpgrade(nodes, sshKey)
-					})
-				})
-			})
-		})
 	})
 })
 
