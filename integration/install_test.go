f678d51cd5d110e659f31e27d8c31a2b9f904214
Alexander Brand <abrand@apprenda.com>
2017-08-01 13:59:59 -0400

    Disable all contiv tests. We have seen them pass, but the flakyness we are getting is not worth it.

--- integration/install_test.go
+++ integration/install_test.go
@@ -288,58 +288,58 @@ var _ = Describe("kismatic", func() {
 			})
 		})
 
-		Context("when deploying a skunkworks cluster", func() {
-			Context("with Contiv as the CNI provider", func() {
-				ItOnAWS("should install successfully [slow]", func(aws infrastructureProvisioner) {
-					WithInfrastructure(NodeCount{3, 2, 3, 2, 2}, Ubuntu1604LTS, aws, func(nodes provisionedNodes, sshKey string) {
-						// reserve one of the workers for the add-worker test
-						allWorkers := nodes.worker
-						nodes.worker = allWorkers[0 : len(nodes.worker)-1]
+		// Context("when deploying a skunkworks cluster", func() {
+		// 	Context("with Contiv as the CNI provider", func() {
+		// 		ItOnAWS("should install successfully [slow]", func(aws infrastructureProvisioner) {
+		// 			WithInfrastructure(NodeCount{3, 2, 3, 2, 2}, Ubuntu1604LTS, aws, func(nodes provisionedNodes, sshKey string) {
+		// 				// reserve one of the workers for the add-worker test
+		// 				allWorkers := nodes.worker
+		// 				nodes.worker = allWorkers[0 : len(nodes.worker)-1]
 
-						// install cluster
-						installOpts := installOptions{
-							heapsterReplicas:    3,
-							heapsterInfluxdbPVC: "influxdb",
-							cniProvider:         "contiv",
-						}
-						err := installKismatic(nodes, installOpts, sshKey)
-						Expect(err).ToNot(HaveOccurred())
+		// 				// install cluster
+		// 				installOpts := installOptions{
+		// 					heapsterReplicas:    3,
+		// 					heapsterInfluxdbPVC: "influxdb",
+		// 					cniProvider:         "contiv",
+		// 				}
+		// 				err := installKismatic(nodes, installOpts, sshKey)
+		// 				Expect(err).ToNot(HaveOccurred())
 
-						sub := SubDescribe("Using a running cluster")
-						defer sub.Check()
+		// 				sub := SubDescribe("Using a running cluster")
+		// 				defer sub.Check()
 
-						sub.It("should allow adding a worker node", func() error {
-							newWorker := allWorkers[len(allWorkers)-1]
-							return addWorkerToCluster(newWorker)
-						})
+		// 				sub.It("should allow adding a worker node", func() error {
+		// 					newWorker := allWorkers[len(allWorkers)-1]
+		// 					return addWorkerToCluster(newWorker)
+		// 				})
 
-						// This test is flaky with contiv
-						// sub.It("should be able to deploy a workload with ingress", func() error {
-						// 	return verifyIngressNodes(nodes.master[0], nodes.ingress, sshKey)
-						// })
+		// 				// This test is flaky with contiv
+		// 				// sub.It("should be able to deploy a workload with ingress", func() error {
+		// 				// 	return verifyIngressNodes(nodes.master[0], nodes.ingress, sshKey)
+		// 				// })
 
-						// Use master[0] public IP
-						// There is an issue with contiv that prevents this test from passing consistently
-						// sub.It("should have an accessible dashboard", func() error {
-						// 	return canAccessDashboard(fmt.Sprintf("https://admin:abbazabba@%s:6443/ui", nodes.master[0].PublicIP))
-						// })
+		// 				// Use master[0] public IP
+		// 				// There is an issue with contiv that prevents this test from passing consistently
+		// 				// sub.It("should have an accessible dashboard", func() error {
+		// 				// 	return canAccessDashboard(fmt.Sprintf("https://admin:abbazabba@%s:6443/ui", nodes.master[0].PublicIP))
+		// 				// })
 
-						// Contiv does not support the Kubernetes network policy API
-						// sub.It("should respect network policies", func() error {
-						// 	return verifyNetworkPolicy(nodes.master[0], sshKey)
-						// })
+		// 				// Contiv does not support the Kubernetes network policy API
+		// 				// sub.It("should respect network policies", func() error {
+		// 				// 	return verifyNetworkPolicy(nodes.master[0], sshKey)
+		// 				// })
 
-						sub.It("should support heapster with persistent storage", func() error {
-							return verifyHeapster(nodes.master[0], sshKey)
-						})
+		// 				sub.It("should support heapster with persistent storage", func() error {
+		// 					return verifyHeapster(nodes.master[0], sshKey)
+		// 				})
 
-						sub.It("should have tiller running", func() error {
-							return verifyTiller(nodes.master[0], sshKey)
-						})
-					})
-				})
-			})
-		})
+		// 				sub.It("should have tiller running", func() error {
+		// 					return verifyTiller(nodes.master[0], sshKey)
+		// 				})
+		// 			})
+		// 		})
+		// 	})
+		// })
 
 		ItOnPacket("should install successfully [slow]", func(packet infrastructureProvisioner) {
 			WithMiniInfrastructure(Ubuntu1604LTS, packet, func(node NodeDeets, sshKey string) {
