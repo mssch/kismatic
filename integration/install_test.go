74a25dffef606fe172f0036cb966a4b67ccce18b
Alexander Brand <abrand@apprenda.com>
2017-11-28 17:21:51 -0500

    Add test coverage for idempotent preflight on all supported OSs

--- integration/install_test.go
+++ integration/install_test.go
@@ -131,6 +131,9 @@ var _ = Describe("kismatic", func() {
 				WithMiniInfrastructure(CentOS7, aws, func(node NodeDeets, sshKey string) {
 					err := installKismaticMini(node, sshKey)
 					Expect(err).ToNot(HaveOccurred())
+					// Ensure preflight checks are idempotent on CentOS7
+					err = runValidate("kismatic-testing.yaml")
+					Expect(err).ToNot(HaveOccurred())
 				})
 			})
 		})
@@ -140,6 +143,9 @@ var _ = Describe("kismatic", func() {
 				WithMiniInfrastructure(RedHat7, aws, func(node NodeDeets, sshKey string) {
 					err := installKismaticMini(node, sshKey)
 					Expect(err).ToNot(HaveOccurred())
+					// Ensure preflight checks are idempotent on RedHat7
+					err = runValidate("kismatic-testing.yaml")
+					Expect(err).ToNot(HaveOccurred())
 				})
 			})
 		})
@@ -148,6 +154,9 @@ var _ = Describe("kismatic", func() {
 			ItOnAWS("should install successfully", func(aws infrastructureProvisioner) {
 				WithMiniInfrastructure(Ubuntu1604LTS, aws, func(node NodeDeets, sshKey string) {
 					err := installKismaticMini(node, sshKey)
+					Expect(err).ToNot(HaveOccurred())
+					// Ensure preflight checks are idempotent on Ubuntu 1604
+					err = runValidate("kismatic-testing.yaml")
 					Expect(err).ToNot(HaveOccurred())
 				})
 			})
