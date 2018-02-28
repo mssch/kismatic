a946277c0308767a39280b57736eb70495fa0035
Dimitri Koshkin <dimitri.koshkin@gmail.com>
2018-02-27 20:08:16 -0500

    Set --kubeconfig on all kubectl commands

--- integration-tests/rbac.go
+++ integration-tests/rbac.go
@@ -20,7 +20,7 @@ func verifyRBAC(master NodeDeets, sshKey string) error {
 	// create using kubectl
 	commands := []string{
 		// create the RBAC policy
-		"sudo kubectl create -f /tmp/pod-reader.yaml",
+		"sudo kubectl --kubeconfig /root/.kube/config create -f /tmp/pod-reader.yaml",
 		// generate a private key for jane
 		"sudo openssl genrsa -out /tmp/jane-key.pem 2048",
 		// generate a CSR for jane
@@ -28,20 +28,20 @@ func verifyRBAC(master NodeDeets, sshKey string) error {
 		// generate certificate for jane
 		"sudo openssl x509 -req -in /tmp/jane-csr.pem -CA /etc/kubernetes/ca.pem -CAkey /tmp/ca-key.pem -CAcreateserial -out /tmp/jane.pem -days 10",
 		// configure new user in kubeconfig
-		"sudo kubectl config set-credentials jane --client-certificate=/tmp/jane.pem --client-key=/tmp/jane-key.pem",
+		"sudo kubectl --kubeconfig /root/.kube/config config set-credentials jane --client-certificate=/tmp/jane.pem --client-key=/tmp/jane-key.pem",
 	}
 	err = runViaSSH(commands, []NodeDeets{master}, sshKey, 30*time.Second)
 	if err != nil {
 		return err
 	}
 	// Using kubectl to get pods should succeed
-	err = runViaSSH([]string{"sudo kubectl get pods --user=jane"}, []NodeDeets{master}, sshKey, 30*time.Second)
+	err = runViaSSH([]string{"sudo kubectl --kubeconfig /root/.kube/config get pods --user=jane"}, []NodeDeets{master}, sshKey, 30*time.Second)
 	if err != nil {
 		return fmt.Errorf("failed to get pods as jane: %v", err)
 	}
 	// This command is expected to fail, so we ignore the error.
 	// We expect the output to contain the string "Forbidden"
-	out, _ := executeCmd("sudo kubectl get nodes --user=jane", master.PublicIP, master.SSHUser, sshKey)
+	out, _ := executeCmd("sudo kubectl --kubeconfig /root/.kube/config get nodes --user=jane", master.PublicIP, master.SSHUser, sshKey)
 	if !strings.Contains(out, "Forbidden") {
 		return fmt.Errorf("expected a forbidden response from the server, but output did not indicate this. Output was: %s", out)
 	}
