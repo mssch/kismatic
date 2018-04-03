ef29e037f0d2c6844ea78282f718eb8f44c1f7fe
Dimitri Koshkin <dimitri.koshkin@gmail.com>
2018-04-02 22:41:17 -0400

    Fix plan file key regex to include /
    A node label or taint may contain a "/", need to match it when reading plan file keys.

--- pkg/install/plan.go
+++ pkg/install/plan.go
@@ -216,7 +216,7 @@ func setDefaults(p *Plan) {
 	}
 }
 
-var yamlKeyRE = regexp.MustCompile(`[^a-zA-Z]*([a-z_\-A-Z.\d]+)[ ]*:`)
+var yamlKeyRE = regexp.MustCompile(`[^a-zA-Z]*([a-z_\-\/A-Z.\d]+)[ ]*:`)
 
 // Write the plan to the file system
 func (fp *FilePlanner) Write(p *Plan) error {
